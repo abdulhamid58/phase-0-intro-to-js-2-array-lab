f3494e25ab8ee662df636305dd3c6345712a411a
Abdulhamid <abdulhamidhersi@gmail.com>
2024-09-23 14:37:05 +0300

    complete

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
+
+
 let cats = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendCat(name) {
-  cats.push(name);
+  cats.push('Ralph');
 }
 
 function destructivelyPrependCat(name) {
-  cats.unshift(name);
+  cats.unshift('Bob');
 }
+
 
 function destructivelyRemoveLastCat() {
   cats.pop();
 }
 
+
 function destructivelyRemoveFirstCat() {
   cats.shift();
+
 }
 
 function appendCat(name) {
-  return cats.concat([name]);
+ return [...cats, 'Broom'];
 }
 
 function prependCat(name) {
-  return [name].concat(cats);
+  return ['Arnold', ...cats];
 }
 
-function removeLastCat() {
-  return cats.slice(0, cats.length - 1);
+function removeLastCat()  {
+  return cats.slice(0, -1);
 }
+ 
 
 function removeFirstCat() {
   return cats.slice(1);
 }
+
+
 
 
